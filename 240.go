de2cfc73402f2238cba0229d30da518385fd4880
RicardoL1u <2018212874@bupt.edu.cn>
2021-09-07 00:47:16 +0800

    240

--- 240.go
+++ 240.go
@@ -1,4 +1,4 @@
-package leetcode
+package main
 
 import (
 	"fmt"
@@ -8,20 +8,63 @@ func searchMatrix(matrix [][]int, target int) bool {
 	sr, sc := 0, 0
 	er := len(matrix)
 	ec := len(matrix[0])
-	fmt.Println(er, " ", ec)
-	fmt.Println(sr, " ", sc)
 
 	for sr < er && sc < ec {
 		mr := (er-sr)/2 + sr
 		mc := (ec-sc)/2 + sc
+		// fmt.Println(sr, sc, er, ec, mr, mc)
 		if matrix[mr][mc] == target {
 			return true
+		} else if matrix[mr][mc] < target {
+			col := copyCol(matrix, mc)
+			m1 := binarySearch(col, target, mc, false)
+			m2 := binarySearch(matrix[mr], target, mr, false)
+			// fmt.Println(m1, m2)
+			if m1 || m2 {
+				return true
+			}
+			sr = mr + 1
+			sc = mc + 1
 		} else {
-
+			col := copyCol(matrix, mc)
+			m1 := binarySearch(col, target, mc, true)
+			m2 := binarySearch(matrix[mr], target, mr, true)
+			// fmt.Println(m1, m2)
+			if m1 || m2 {
+				return true
+			}
+			er = mr
+			ec = mc
 		}
 	}
 
 	return false
+}
+
+// var cnt int = 0
+
+func binarySearch(nums []int, target, p int, isBig bool) bool {
+	// cnt := 0
+	s := 0
+	e := len(nums)
+	if isBig {
+		e = p
+	} else {
+		s = p + 1
+	}
+	for s < e {
+		m := (e-s)/2 + s
+		if nums[m] < target {
+			s = m + 1
+		} else {
+			e = m
+		}
+	}
+	if s >= len(nums) {
+		return false
+	} else {
+		return nums[s] == target
+	}
 }
 
 func copyCol(mat [][]int, col int) []int {
@@ -43,4 +86,5 @@ func main() {
 	// fmt.Println(mat[][1])
 	fmt.Println(copyCol(mat, 1))
 	// fmt.Println(searchMatrix(mat, 4))
+	fmt.Println(searchMatrix(mat, 4))
 }
