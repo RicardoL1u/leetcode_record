51355369f814ee9c542d5cdac4f24f4c79624f1e
v_ytaoliu <v_ytaoliu@tencent.com>
2021-12-01 20:08:28 +0800

    33.go:dp

--- 33.go
+++ 33.go
@@ -1,88 +1,70 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func search(nums []int, target int) int {
-	oriNums := make([]int,5010)
-	n := len(nums)
-	if n == 1{
-		if nums[0] != target{
-			return -1
-		} else {
-			return 0
+func longestValidParentheses1(s string) int {
+	dp := make([][]int, len(s)+2)
+	for k := range dp {
+		dp[k] = make([]int, len(s)+2)
+	}
+	ans := 0
+	for i := 1; i <= len(s); i++ {
+		for j := i; j <= len(s); j++ {
+			if s[j-1] == '(' {
+				if dp[i][j-1] >= 0 {
+					dp[i][j] = dp[i][j-1] + 1
+				} else {
+					break
+				}
+			} else {
+				dp[i][j] = dp[i][j-1] - 1
+			}
+			if dp[i][j] == 0 {
+				ans = max(ans, j-i+1)
+			}
 		}
 	}
-	k := findRotatedPoi(nums)
-	copy(oriNums[0:n-k],nums[k:])
-	copy(oriNums[n-k:],nums[0:k])
-	// k := 10
-	// fmt.Println(oriNums[0:n])
-	oriPos := findTarget(oriNums,target,n)
-	if oriPos == -1 {
-		return oriPos
-	}
-	nowPos := getOriPos(oriPos,n,k)
-	// fmt.Println(oriPos, " ", nowPos)
-	return nowPos
+	return ans
 }
 
-func getOriPos (oriPos,n,k int) int{
-	res := 0
-	if oriPos < n - k {
-		res = oriPos + k
+func longestValidParentheses(s string) int {
+	ans := 0
+	temp := 0
+	for i := 1; i <= len(s); i++ {
+		temp = 0
+		for j := i; j <= len(s); j++ {
+			if s[j-1] == '(' {
+				if temp >= 0 {
+					temp++
+				} else {
+					break
+				}
+			} else {
+				temp--
+			}
+			if temp == 0 {
+				ans = max(ans, j-i+1)
+			}
+		}
+		// fmt.Println(dp)
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
 	} else {
-		res = oriPos - (n - k)
-	}
-	return res
-}
-
-func findTarget(nums[] int,target int,l int) int {
-	s := 0
-	e := l
-	cmpVal := target
-	for s < e {
-		p := (e - s) / 2 + s
-		// fmt.Println(s," ",e," ",p)
-		if cmpVal > nums[p]{
-			s = p + 1
-		} else {
-			e = p
-		}
-	}
-	if nums[s] == cmpVal {
-		return s
-	} else {
-		return -1
+		return b
 	}
 }
-
-func findRotatedPoi(nums[] int) int {
-	s := 0
-	e := len(nums)
-	cmpVal := nums[e - 1]
-	for s < e {
-		p := (e - s) / 2 + s
-		if cmpVal < nums[p]{
-			s = p + 1
-		} else {
-			e = p
-		}
-	}
-	return s
-}
-
-func main(){
-
-	// nums := []int{4,5,6,7,0,1,2}
-	// nums := []int{11,22,33,44,55,3,5,7}
-	// nums := []int{1}
-	a := []int{4,5,6,7,0,1,2}
-	// b := make([]int,10)
-	// cp := copy(b[4:6],a[1:3])
-	// fmt.Println(cp)
-	// fmt.Println(b)
-	// fmt.Println(findRotatedPoi(nums))
-	fmt.Println(search(a,1))
+func main() {
+	// s := ")()())"
+	fmt.Println(longestValidParentheses(")()())") == 4)
+	fmt.Println(longestValidParentheses(")()(()))") == 6)
+	fmt.Println(longestValidParentheses(")") == 0)
+	fmt.Println(longestValidParentheses("") == 0)
+	fmt.Println(longestValidParentheses(")))))(((((((") == 0)
+	fmt.Println(longestValidParentheses(")))))((((((())))") == 8)
+	fmt.Println(longestValidParentheses("()") == 2)
 }
