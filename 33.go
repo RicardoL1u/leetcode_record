6ef5b7f388433f68f5c2667942a56576a2363b25
v_ytaoliu <v_ytaoliu@tencent.com>
2021-12-03 17:04:14 +0800

    33.go:n^2 solution

--- 33.go
+++ 33.go
@@ -27,7 +27,7 @@ func longestValidParentheses1(s string) int {
 	return ans
 }
 
-func longestValidParentheses(s string) int {
+func longestValidParentheses2(s string) int {
 	ans := 0
 	temp := 0
 	for i := 1; i <= len(s); i++ {
@@ -51,6 +51,36 @@ func longestValidParentheses(s string) int {
 	return ans
 }
 
+func longestValidParentheses(s string) int {
+	i, j, p, ans := 0, 0, 0, 0
+	stack := make([]byte, len(s))
+	for i < len(s) && j < len(s) && i <= j {
+		if s[j] == '(' {
+			stack[p] = '('
+			p++
+		} else {
+			if p == 0 {
+				j++
+				i = j
+				p = 0
+				continue
+			} else {
+				p--
+			}
+		}
+		if p == 0 {
+			ans = max(ans, j-i+1)
+		}
+	}
+	for i < len(s) {
+		if s[i] == '('{
+			i++
+			if p == i
+		}
+	}
+	return ans
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -60,11 +90,16 @@ func max(a, b int) int {
 }
 func main() {
 	// s := ")()())"
+	fmt.Println(longestValidParentheses("(()(((()"))
+	fmt.Println(longestValidParentheses(")()(()") == 2)
 	fmt.Println(longestValidParentheses(")()())") == 4)
+	fmt.Println(longestValidParentheses(")()(()"))
 	fmt.Println(longestValidParentheses(")()(()))") == 6)
 	fmt.Println(longestValidParentheses(")") == 0)
+	fmt.Println(longestValidParentheses(")(") == 0)
 	fmt.Println(longestValidParentheses("") == 0)
 	fmt.Println(longestValidParentheses(")))))(((((((") == 0)
 	fmt.Println(longestValidParentheses(")))))((((((())))") == 8)
+	fmt.Println(longestValidParentheses(")))))((((((())))"))
 	fmt.Println(longestValidParentheses("()") == 2)
 }
--- test.go
+++ test.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
-func testPtr(b *int) {
-	fmt.Print(b)
-}
-
 func main() {
-	a := 10
-	b := &a
-	testPtr(b)
-	fmt.Println(b)
+	// 判断map中元素是否存在
+	// 兼容0
+	test := map[int]int{1: 1, 2: 2, 3: 0}
+	for i := 0; i < 4; i++ {
+		if k, ok := test[i]; ok {
+			fmt.Println(k, ok)
+		} else {
+			fmt.Println(k, ok)
+		}
+	}
 }
